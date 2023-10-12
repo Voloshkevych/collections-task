75b08853e56a7c71cafc7e61d73e275e42494b68
Voloshkevych <voloshkevych.b@gmail.com>
2023-10-12 13:51:20 +0300

    Second try

--- src/main/java/co/inventorsoft/academy/collections/model/Range.java
+++ src/main/java/co/inventorsoft/academy/collections/model/Range.java
@@ -2,142 +2,150 @@ package co.inventorsoft.academy.collections.model;
 
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 import java.util.Set;
-import java.util.TreeSet;
 import java.util.function.Function;
 
 public class Range<T extends Comparable<T>> implements Set<T> {
 
-    private TreeSet<T> elements;
+    public static void main(String[] args) {
+        Range range = Range.of(Integer.MIN_VALUE + 1, Integer.MAX_VALUE - 1);
+    }
 
-    // factory method to create a Range object with start and end points
+    private T start;
+    private T end;
+    private Function<T, T> function;
+
+    // method to create a Range object with start and end points
     public static <T extends Comparable<T>> Range<T> of(T start, T end) {
         return new Range<>(start, end);
     }
 
-    // factory method to create a Range object with custom function for incrementation
+    // method to create a Range object with custom function for incrementation
     public static <T extends Comparable<T>> Range<T> of(T start, T end, Function<T, T> function) {
         return new Range<>(start, end, function);
     }
 
-    // constructor used for regular start and end points
     private Range(T start, T end) {
-        elements = new TreeSet<>();
-
-        // validate that start is not greater than end
-        if (start.compareTo(end) > 0) {
-            throw new IllegalArgumentException("Start cannot be greater than end");
-        }
-
-        // if start equals end, return an empty set
-        if (start.equals(end)) {
-            return;
-        }
-
-        // populate set based on type by default
-        if (start instanceof Integer) {
-            for (Integer i = (Integer) start; i <= (Integer) end; i++) {
-                elements.add((T) i);
-            }
-        } else if (start instanceof Double) {
-            for (Double i = (Double) start; i <= (Double) end; i += 0.1d) {
-                elements.add((T) i);
-            }
-        } else if (start instanceof Float ) {
-            for (Float i = (Float) start; i <= (Float) end; i += 0.1f) {
-                elements.add((T) i);
-            }
-        } else if (start instanceof Byte) {
-            for (Byte i = (Byte) start; i <= (Byte) end; i++) {
-                elements.add((T) i);
-            }
-        } else if (start instanceof Short) {
-            for (Short i = (Short) start; i <= (Short) end; i++) {
-                elements.add((T) i);
-            }
-        } else if (start instanceof Character) {
-            for (Character i = (Character) start; i <= (Character) end; i++) {
-                elements.add((T) Character.valueOf(i));
-            }
-        } else {
-            throw new IllegalArgumentException("Unsupported type: " + start.getClass().getName());
-        }
-
+        this.start = start;
+        this.end = end;
+        this.function = null;
     }
 
-    // constructor used for custom function for increments
     private Range(T start, T end, Function function) {
-        elements = new TreeSet<>();
-
-        // validate that start is not greater than end
-        if (start.compareTo(end) > 0) {
-            throw new IllegalArgumentException("Start cannot be greater than end");
-        }
-
-        // if start equals end, return an empty set
-        if (start.equals(end)) {
-            return;
-        }
-
-        // populating set using custom function
-        if (function != null) {
-            T current = start;
-            while (current.compareTo(end) <= 0) {
-                elements.add(current);
-                current = (T) function.apply(current);
-            }
-        }
+        this.start = start;
+        this.end = end;
+        this.function = function;
     }
 
     public int size() {
-        return elements.size();
+        int count = 0;
+        for (T t : this) {
+            count++;
+        }
+        return count;
     }
 
     public boolean isEmpty() {
-        return elements.isEmpty();
+        return start.compareTo(end) == 0;
     }
 
     public boolean contains(Object o) {
-        return elements.contains(o);
+        if (o == null || !(o instanceof Comparable)) {
+            return false;
+        }
+
+        T other = (T) o;
+        return other.compareTo(start) >= 0 && other.compareTo(end) <= 0;
     }
 
     public Iterator<T> iterator() {
-        return elements.iterator();
+
+        return new Iterator<T>() {
+            private T current = start;
+
+            @Override
+            public boolean hasNext() {
+                return current.compareTo(end) <= 0;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+
+                T result = current;
+                current = function != null ? function.apply(current) : (T) increaseDefault(current);
+                return result;
+            }
+
+            private T increaseDefault(T elem) {
+                Number num = (Number) elem;
+                if (elem instanceof Double) {
+                    return (T) Double.valueOf(num.doubleValue() + 0.1d);
+                } else if (elem instanceof Float) {
+                    return (T) Float.valueOf(num.floatValue() + 0.1f);
+                } else if (elem instanceof Long) {
+                    return (T) Long.valueOf(num.longValue() + 1);
+                } else if (elem instanceof Integer) {
+                    return (T) Integer.valueOf(num.intValue() + 1);
+                } else if (elem instanceof Short) {
+                    return (T) Short.valueOf((short) (num.shortValue() + 1));
+                } else {
+                    return (T) Byte.valueOf((byte) (num.byteValue() + 1));
+                }
+            }
+        };
     }
 
+    @Override
     public Object[] toArray() {
         return new Object[0];
     }
 
+    @Override
     public <T1> T1[] toArray(T1[] a) {
         return null;
     }
 
+    @Override
     public boolean add(T t) {
-        return elements.add(t);
+        return false;
     }
 
+    @Override
     public boolean remove(Object o) {
-        return elements.remove(o);
+        return false;
     }
 
     public boolean containsAll(Collection<?> c) {
-        return elements.containsAll(c);
+        for (Object elem : c) {
+            if (!contains(elem)) {
+                return false;
+            }
+        }
+        return true;
     }
 
+    @Override
     public boolean addAll(Collection<? extends T> c) {
-        return elements.addAll(c);
+        return false;
     }
 
+    @Override
     public boolean retainAll(Collection<?> c) {
-        return elements.retainAll(c);
+        return false;
     }
 
+    @Override
     public boolean removeAll(Collection<?> c) {
-        return elements.removeAll(c);
+        return false;
     }
 
+    @Override
     public void clear() {
-        elements.clear();
+
     }
+
 }
